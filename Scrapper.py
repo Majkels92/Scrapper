bb1a58e41fa2458405800d16e581721593e8922f
Majkels92 <michal.skowr92@gmail.com>
2022-12-14 14:06:17 +0100

    Get price

--- Scrapper.py
+++ Scrapper.py
@@ -1,23 +1,29 @@
-import requests, bs4
+import requests
+import bs4
 
 
-url_instance = "https://www.amazon.pl/Microsoft-512GB-Edycja-Cyfrowa-Konsola/dp/B08K3BRFT3/ref=sr_1_5?crid=1U1UGL4TV1HWJ&keywords=xbox+series+s&qid=1670063054&sprefix=xbox%2Caps%2C128&sr=8-5"
+url_instance = "https://www.amazon.pl/Microsoft-Xbox-1TB-Konsola-Czarna/dp/B08K3F3VQG/ref=sr_1_5?__mk_pl_PL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2UKXQKV1XWGUC&keywords=xbox+series+x&qid=1671021734&sprefix=xbox+series+x%2Caps%2C175&sr=8-5"
 
-
-def get_product_name(soup):
-    title = soup.find("span", attrs={"id":'productTitle'})
-    return title.string.strip()
 
 if __name__ == '__main__':
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
-    #url = input("wklej link do przedmiotu: \n")
+    # url = input("wklej link do przedmiotu: \n")
     respond = requests.get(url_instance, headers=headers)
-    doc = bs4.BeautifulSoup(respond.content, "html.parser")
-    print(doc.prettify())
-    title = get_product_name(doc)
+    doc = bs4.BeautifulSoup(respond.text, "html.parser")
 
+    page_titile = doc.title
 
+    title = doc.find("span", attrs={"id": 'productTitle'})
 
+    prices = doc.find_all(text="zł")
+    parent = prices[0].parent.parent
+    price_whole = parent.find_all('span')[0]
+    price_decimal = parent.find_all('span')[2]
 
-    print("Title:", title)
+    print("Amazon page info:", page_titile.string.strip())
+    print("Title:", title.text.strip())
+    print("Price:", price_whole.text + price_decimal.text, "zł")
+
+    # print(doc.prettify())
+
