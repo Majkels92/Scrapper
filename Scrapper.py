487efec1ec8f3326f57df0a70bf14f6002d66d43
Majkels92 <michal.skowr92@gmail.com>
2022-12-21 20:34:13 +0100

    getting 1 list object

--- Scrapper.py
+++ Scrapper.py
@@ -2,7 +2,7 @@ import requests
 import bs4
 
 
-url_instance = "https://www.amazon.pl/Microsoft-Xbox-1TB-Konsola-Czarna/dp/B08K3F3VQG/ref=sr_1_5?__mk_pl_PL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2UKXQKV1XWGUC&keywords=xbox+series+x&qid=1671021734&sprefix=xbox+series+x%2Caps%2C175&sr=8-5"
+url_instance = "https://www.amazon.pl/horsemen-RRT-00007-Xbox-Serie-X/dp/B08H93GKNJ/ref=sr_1_omk_6?__mk_pl_PL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=ZVVHNIH2YCFY&keywords=xbox+series+x&qid=1671646584&sprefix=xbox+series+x%2Caps%2C147&sr=8-6"
 
 
 if __name__ == '__main__':
@@ -28,3 +28,14 @@ if __name__ == '__main__':
 
     # print(doc.prettify())
 
+    # det1 = doc.find(["div"], id="detailBullets_feature_div")
+    ullist = doc.find_all(["ul"], class_="a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list")
+
+
+
+li_element = ullist[0].find("li").text.split()
+li_2 = ""
+for i in li_element:
+        li_2 += i + " "
+
+print(li_2)
